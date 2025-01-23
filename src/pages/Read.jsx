a8a40d13c48d1f6ff3349424ad2f331da1aa4d2e
Toqa Alaa <125225693+toqa-alaa@users.noreply.github.com>
2025-01-23 15:18:29 +0200

    my  third edit

--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -1,12 +1,13 @@
 import PageFooter from "../compoments/PageFooter"
 import PageNav from "../compoments/PageNav"
+import Console from "../pages/Console"
 
 function Read() {
     return (
         <div className="background">
             
             <PageNav/>
-
+     <Console/>
             <PageFooter/>
        
         </div>
